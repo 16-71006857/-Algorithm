9d9e234899041be7374e755b44e109f843763be1
71006857 <qutozx@gmail.com>
2019-11-17 17:44:25 +0900

    Update list.c

--- list.c
+++ list.c
@@ -1,3 +1,17 @@
 //test--1
 
 #include <stdio.h>
+ 
+ //선형 리스트 구현
+
+int main()
+{
+	int i = 0;
+	array[10] = {1,2,3,4,5,6,7,8,9};
+
+	for(i=0;i<sizeof(array);i++){
+		printf("index : %d -- address : %u -- value : %d \n",i,&array[i],array[i]);
+	}
+
+	return 0;
+}
